f18a52a12897e7dba96f422b16ff4bb8bfd98447
Dennis Keefe <dkeefe@tutanota.com>
2019-05-24 11:40:35 -0500

    modified changelog and version number

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,3 +1,4 @@
 # ChangeLog
 
 * (5 November 2014). Added Base Plugin dependency.
+* (22 May 2019). Removed the text "all kids" from the "Back to All Kids" button
--- purecharity-wp-sponsorships.php
+++ purecharity-wp-sponsorships.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Pure Charity Sponsorships
  * Plugin URI:        http://purecharity.com/
  * Description:       Plugin to display a list of or a single sponsorship program from the Pure Charity app.
- * Version:           1.5
+ * Version:           1.6
  * Author:            Pure Charity
  * Author URI:        http://purecharity.com/
  * License:           GPL-2.0+
